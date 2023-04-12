2c5b024df48c9e54ba045f1784a7c82fa1d0ab51
Pedro Franch <pedrofe.av@gmail.com>
2023-04-12 10:12:14 +0200

    8.2

--- ejemploT8.js
+++ ejemploT8.js
@@ -1,13 +1,34 @@
 function buscarEnlaces(){
     //let enlace = document.getElementById('Colegio');
-    let enlace = document.querySelector('#Colegio');
-    alert(enlace.href);
+    //let enlace = document.querySelector('#Colegio');
+    //alert(enlace.href);
 
 
-    let parrafos = document.querySelectorAll('a+p');
-    for(let parr of parrafos){
-        alert(parr.innerHTML);
+    //let parrafos = document.querySelectorAll('a+p');
+    //for(let parr of parrafos){
+      //  alert(parr.innerHTML);
+    //}
+
+    let enlaces = document.querySelectorAll('a'); //acede al siguiente elemento despues de un enalce
+    for(let enlace of enlaces){
+        enlace.nextElementSibling.innerHTML = "hola hola"
+
+        let siguienteParafo = enlace.nextElementSibling; //coje el siguiente elemento "hermano" del enlace del harray
+        let primerHijodelParafo = siguienteParafo.firstChild; //coje el primer hijo del elemento, es decir esta dentro de este
+        
     }
 }
 
-buscarEnlaces();
+//buscarEnlaces();
+
+function crearParafo(contenido){
+  let parrafonuevo = document.createElement('p');
+  parrafonuevo.innerHTML = contenido;
+
+  /*let elbody = document.querySelector('body');
+  elbody.append(parrafonuevo);*/
+
+  document.body.append(parrafonuevo);
+}
+
+crearParafo(prompt("Introduce el contenido del párrafo"));
